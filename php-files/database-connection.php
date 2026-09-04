c374b6457e2077f659116da22fe56b85c8f0306d
Sephanie-2005 <ualatmariasephanie.gmail.com>
2026-09-04 17:11:58 +0000

    PDO database instance connection created

---
+++ php-files/database-connection.php
@@ -0,0 +1,21 @@
+<?php
+
+    $host     = 'localhost';
+    $db_name  = 'uhoppy_db'; 
+    $username = 'root';     
+    $password = '';         
+
+    try {
+        $dsn = "mysql:host=$host;dbname=$db_name;charset=utf8mb4";
+        $pdo = new PDO($dsn, $username, $password, [
+            PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION, 
+            PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,       
+            PDO::ATTR_EMULATE_PREPARES   => false,                  
+        ]);
+    } 
+
+    catch (PDOException $e) {
+        // If the database fails to link, display a clean error message
+        die("Database connection failed: " . $e->getMessage());
+    }
+?>
